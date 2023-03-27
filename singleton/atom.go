666c30f50c0c6e3c9ac61957d9387438675ca3a5
Prashanth6361 <prashanthraj.15.2000@gmail.com>
2023-03-27 16:30:36 +0530

    prashanth

--- singleton/atom.go
+++ singleton/atom.go
@@ -6,14 +6,13 @@ import (
 	"time"
 )
 
-var x int32 = 0
-
-func atomicLoad() {
-	atomic.AddInt32(&x, 1)
+func atomicLoad(x *int32) {
+	atomic.AddInt32(x, 1)
 }
 func main() {
+	var x int32
 	for i := 0; i < 10; i++ {
-		go atomicLoad()
+		go atomicLoad(&x)
 	}
 	time.Sleep(2 * time.Second)
 	fmt.Println("value of x:", atomic.LoadInt32(&x))
