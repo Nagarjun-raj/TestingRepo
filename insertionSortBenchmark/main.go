057cd8b37482502c566983ef989d073b34b86c11
Nagarjun-raj <nagarjunathreyasa@gmail.com>
2023-04-13 13:01:15 +0530

    Nagarjun

--- insertionSortBenchmark/main.go
+++ insertionSortBenchmark/main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func insertion(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
-		j := i - 1
-		for ; j >= 0; j-- {
+		for j:=i-1; j >= 0; j-- {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
